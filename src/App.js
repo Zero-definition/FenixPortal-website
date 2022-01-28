179cc4a19a2d137d7f3987cac584c18f3af24d30
SleepyPrajj <88706712+SleepyPrajj@users.noreply.github.com>
2022-01-28 15:34:59 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar title="Monke" aboutText="chungus" />
+      <div className="container">
+      </div>
+    </>
+  );
+}
+
+export default App;
